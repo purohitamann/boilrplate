68e0799c578c463bb1681aa3b36122c26d67a6aa
Aman Purohit <122639076+purohitamann@users.noreply.github.com>
2025-04-19 23:38:11 -0400

    update .gitignore and modify createFullstackApp function for improved backend setup

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
+process.env.ENVIRONMENT = 'production';
+process.env.NEXT_PUBLIC_BACKEND_URL = 'manicode-backend.onrender.com';
+process.env.NEXT_PUBLIC_APP_URL = 'https://codebuff.com';
+process.env.NEXT_PUBLIC_SUPPORT_EMAIL = 'support@codebuff.com';
 const { createFullstackApp } = require('../lib/create');
 
 const args = process.argv.slice(2);
 const appName = args[0] || 'app';
-createFullstackApp(appName, 'express');
+createFullstackApp(appName, 'menn');
+
+
+
+// GOAL
+
+// npx boilrplate "I want to create a Stitic protfolio using next"
